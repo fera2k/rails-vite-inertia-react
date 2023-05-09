d9f8c203a43ab0c1ad690a8aa1fc2b1ac1d4854d
Fernando Vieira <fernando.vieira@homelight.com>
2023-05-08 23:21:18 -0300

    adding minWidth to Layout

--- app/frontend/layouts/SimpleLayout.tsx
+++ app/frontend/layouts/SimpleLayout.tsx
@@ -5,7 +5,7 @@ import NavHeader from '@/pages/Nav/NavHeader';
 import SmallFooterWithSocial from '../components/Footer';
 
 const SimpleLayout = ({ children }: { children: ReactNode }) => (
-  <Flex direction="column" height="100vh" className="bg-gray-100">
+  <Flex direction="column" height="100vh" className="bg-gray-100" minWidth={360}>
     <NavHeader />
     <Box padding={[2, 6]}>{children}</Box>
     <Spacer />
