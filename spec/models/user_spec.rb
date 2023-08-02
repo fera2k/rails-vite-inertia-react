96f4cb9e82c54e5b2309968e22aaf41da198ac2e
Fernando Vieira <fvieira@estee.com>
2023-08-02 12:41:01 -0300

    add more model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe User do
     user.save
     expect(described_class.find(user.id)).to eq(user)
   end
+
+  it 'has been correctly loaded' do
+    user.save
+    user2 = described_class.find(user.id)
+    expect(user2).to eq(user)
+  end
+
+  it 'has been correckly updated' do
+    user.save
+    user.update(username: 'new name')
+    expect(user.username).to eq('new name')
+  end
+
+  it 'has been correckly deleted' do
+    user.save
+    user.destroy
+    expect(described_class.find_by(id: user.id)).to be_nil
+  end
+
+  it 'has been able to update password' do
+    user.save
+    user.update(password: 'new password')
+    expect(user.password).to eq('new password')
+  end
 end
